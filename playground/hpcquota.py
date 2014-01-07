c5867d3dbf0232c957a8b505b6faf0b12acd7836
perillaroc <perillaroc@gmail.com>
2014-01-07 21:48:53 +0800

    use json in hpcquota
    create output for cmquota

--- playground/hpcquota.py
+++ playground/hpcquota.py
@@ -2,7 +2,6 @@ import paramiko
 import re
 import string
 
-
 def get_cmquota_by_user(hostname,port,username,password):
     bin_path = '/cma/u/app/sys_bin/cmquota'
     bin_param = username
@@ -18,26 +17,43 @@ def get_cmquota_by_user(hostname,port,username,password):
     result_lines = stdout.read().split("\n")
     ssh.close()
 
-    a_pattern = r'^(\w+) +(\w+) +(\d+) +(\d+) +(\d+) +(\d+) +.*'
-    prog = re.compile(a_pattern)
+    detail_pattern = r'^(\w+) +(\w+) +(\d+) +(\d+) +(\d+) +(\d+) +.*'
+    detail_prog = re.compile(detail_pattern)
+    quota_result = dict()
+    file_system_list = list()
     for a_line in result_lines:
-        re_result = prog.match(a_line)
-        if re_result:
-            file_system = re_result.group(1)
-            type = re_result.group(2)
-            current_usage = re_result.group(3)
+        detail_re_result = detail_prog.match(a_line)
+        if detail_re_result:
+            file_system = detail_re_result.group(1)
+            quota_type = detail_re_result.group(2)
+            current_usage = detail_re_result.group(3)
             if current_usage.isdigit():
                 current_usage = string.atol(current_usage)
-            soft_limit = re_result.group(4)
+            soft_limit = detail_re_result.group(4)
             if soft_limit.isdigit():
                 soft_limit = string.atol(soft_limit)
-            hard_limit = re_result.group(5)
+            hard_limit = detail_re_result.group(5)
             if hard_limit.isdigit():
                 hard_limit = string.atol(hard_limit)
-            print "%.2f%%,%.2f%%" % (float(current_usage)/soft_limit*100,
-                                     float(current_usage)/hard_limit*100)
+            #print "%s : %.2f%%,%.2f%%" % (file_system,
+            #                              float(current_usage)/soft_limit*100,
+            #                              float(current_usage)/hard_limit*100)
+
+            current_file_system = dict()
+            current_file_system['file_system'] = file_system
+            current_file_system['quota_type'] = quota_type
+            current_file_system['current_usage'] = current_usage
+            current_file_system['soft_limit'] = soft_limit
+            current_file_system['hard_limit'] = hard_limit
+
+            current_file_system['node_type'] = 'detail'
+
+            current_file_system['raw_line'] = a_line
+            file_system_list.append(current_file_system)
+
+    quota_result['file_systems'] = file_system_list
     import json
-    dump = json.dumps(result_lines)
+    dump = json.dumps(quota_result)
     return dump
 
 if __name__ == "__main__":
@@ -46,4 +62,4 @@ if __name__ == "__main__":
     username = 'wangdp'
     password = 'perilla'
 
-    get_cmquota_by_user(hostname,port,username,password)
+    print get_cmquota_by_user(hostname,port,username,password)
