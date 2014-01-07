5abf62ea028a480a779ebea7ea205779d804adf7
perillaroc <perillaroc@gmail.com>
2014-01-07 22:23:28 +0800

    add work index
    add route for /work

--- windroc_site.py
+++ windroc_site.py
@@ -1,9 +1,14 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, url_for
 app = Flask(__name__)
+
+@app.route('/work')
+def work_index():
+    return render_template('work.html')
 
 @app.route('/')
 def hello_world():
     return render_template('index.html')
 
 if __name__ == '__main__':
+    app.debug = True
     app.run()
