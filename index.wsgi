4d9f6eb706c9a88c6f6a6fdc1b14c1e3057bfc38
perillaroc <perillaroc@gmail.com>
2015-05-07 15:13:52 +0800

    CHANGE: port no to 5100.

--- index.wsgi
+++
@@ -1,4 +0,0 @@
-import sys
-sys.path.insert(0, 'D:/project/web/python/local/nwp/mysite')
-
-from windroc_site import app as application
--- run_server.py
+++ run_server.py
@@ -3,7 +3,7 @@ from nwpc_operation_web_monitor import app
 
 
 def runserver():
-    port = int(os.environ.get('PORT', 6000))
+    port = int(os.environ.get('PORT', 5100))
     app.run(host='0.0.0.0', port=port)
 
 if __name__ == '__main__':
